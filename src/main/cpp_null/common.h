164eb10713515faf629d0977e2e952583848b5b8
michalnand <michal.nand@gmail.com>
2016-12-26 15:42:40 +0100

    some boards changes

---
+++ src/main/cpp_null/common.h
@@ -0,0 +1,18 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
+#include <unistd.h>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <cstdint>
+#include <cstring>
+
+#include <string>
+
+
+#endif
---
+++ src/main/cpp_null/main.cpp
@@ -0,0 +1,7 @@
+#include <common.h>
+
+int main()
+{
+  printf("program done\n");
+  return 0;
+}
